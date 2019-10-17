f025cd1daa3f6aefc5ded36acde1ebd1a94ec5d0
Iga <iga.naskret@gmail.com>
2019-10-17 10:38:00 +0200

    Updates

---
+++ precache-manifest.94ddad1b57da2fd181ea0d846e69fc2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2456078326e20973d98caaa313d633c6",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "dc8d40c5c2494d50f068",
+    "url": "/reactjs/static/css/main.3087386b.chunk.css"
+  },
+  {
+    "revision": "0dc992bdf4f23324fa8a",
+    "url": "/reactjs/static/js/2.e8eba799.chunk.js"
+  },
+  {
+    "revision": "dc8d40c5c2494d50f068",
+    "url": "/reactjs/static/js/main.a510894a.chunk.js"
+  },
+  {
+    "revision": "e45d5898817c7650f30c",
+    "url": "/reactjs/static/js/runtime-main.4081ecd2.js"
+  }
+]);
